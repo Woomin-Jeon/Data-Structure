f4552b9177e9f8a1c39dd67b06be045064063486
woomin <dal96k@gmail.com>
2020-04-29 10:38:09 +0900

    Do refactoring depthFirstSearch.test.js

--- depthFirstSearch.test.js
+++ depthFirstSearch.test.js
@@ -7,19 +7,28 @@ class DepthFirstSearch extends Graph {
   }
 
   dfsMethod(startValue) {
+    const alreadyVisited = new Map();
+
     const recursive = (startValue) => {
       const child = this.edges[startValue];
-      if(!child) {
+
+      if(alreadyVisited.get(startValue)) {
         return;
       }
+
       this.searchPath.push(startValue);
-      for(let i = 0; i<child.length; i++) {
+      alreadyVisited.set(startValue, 'exist');
+
+      if (!child) {
+        return;
+      }
+
+      for (let i = 0; i < child.length; i++) {
         recursive(child[i]);
       }
     }
     recursive(startValue);
 
-    this.searchPath.shift();
     const result = this.searchPath;
     this.searchPath = [];
     return result;
@@ -27,64 +36,47 @@ class DepthFirstSearch extends Graph {
 }
 
 describe('depthFirstSearch', () => {
-  test('dFS', () => {
-    const depthFirstSearch = new DepthFirstSearch();
+  describe('with existent direction', () => {
+    it('returns search path', () => {
+      const depthFirstSearch = new DepthFirstSearch();
 
-    depthFirstSearch.addDirectedEdge('A', 'B');
-    depthFirstSearch.addDirectedEdge('B', 'D');
-    depthFirstSearch.addDirectedEdge('D', 'G');
-    depthFirstSearch.addDirectedEdge('B', 'E');
-    depthFirstSearch.addDirectedEdge('E', 'H');
-    depthFirstSearch.addDirectedEdge('A', 'C');
-    depthFirstSearch.addDirectedEdge('C', 'F');
-    
-    expect(depthFirstSearch.dfsMethod('A')).toEqual(['B', 'D' ,'G', 'E', 'H', 'C', 'F']);
-    // expect(depthFirstSearch.dfsMethod('B')).toEqual(['D', 'G' ,'E', 'H']);
+      depthFirstSearch.addDirectedEdge('A', 'B');
+      depthFirstSearch.addDirectedEdge('B', 'D');
+      depthFirstSearch.addDirectedEdge('D', 'G');
+      depthFirstSearch.addDirectedEdge('B', 'E');
+      depthFirstSearch.addDirectedEdge('E', 'H');
+      depthFirstSearch.addDirectedEdge('A', 'C');
+      depthFirstSearch.addDirectedEdge('C', 'F');
+      
+      expect(depthFirstSearch.dfsMethod('A')).toEqual(['A', 'B', 'D' ,'G', 'E', 'H', 'C', 'F']);
+      expect(depthFirstSearch.dfsMethod('B')).toEqual(['B', 'D', 'G' ,'E', 'H']);
+
+      depthFirstSearch.addDirectedEdge('E', 'C');
+      expect(depthFirstSearch.dfsMethod('B')).toEqual(['B', 'D', 'G' ,'E', 'H', 'C', 'F']);
+    });
+  });
+  
+  describe('with unexistent direction', () => {
+    it('returns search path', () => {
+      const depthFirstSearch = new DepthFirstSearch();
+
+      depthFirstSearch.addEdge('A', 'B');
+      depthFirstSearch.addEdge('B', 'D');
+      depthFirstSearch.addEdge('D', 'G');
+      depthFirstSearch.addEdge('B', 'E');
+      depthFirstSearch.addEdge('E', 'H');
+      depthFirstSearch.addEdge('A', 'C');
+      depthFirstSearch.addEdge('C', 'F');
+
+      expect(depthFirstSearch.dfsMethod('A')).toEqual(['A', 'B', 'D' ,'G', 'E', 'H', 'C', 'F']);
+      expect(depthFirstSearch.dfsMethod('D')).toEqual(['D', 'B', 'A', 'C', 'F', 'E', 'H', 'G']);
+      
+      depthFirstSearch.addEdge('E', 'C')
+      expect(depthFirstSearch.dfsMethod('D')).toEqual(['D', 'B', 'A', 'C', 'F', 'E', 'H', 'G']);
+      depthFirstSearch.addEdge('H', 'F')
+      expect(depthFirstSearch.dfsMethod('D')).toEqual(['D', 'B', 'A', 'C', 'F', 'E', 'H', 'G']);
+      depthFirstSearch.addEdge('F', 'G')
+      expect(depthFirstSearch.dfsMethod('D')).toEqual(['D', 'B', 'A', 'C', 'F', 'G', 'E', 'H']);
+    });
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const recursive = (startValue) => {
-//       const child = this.edges[startValue];
-//       this.searchPath.push(startValue);
-  
-//       if (!child) {
-//         return ;
-//       }
-
-//       for (let i = 0; i < child.length; i++) {
-//         recursive(child[i]);
-//       }
-//     }
-//     recursive(startValue);
-
-    // this.searchPath.shift();
-    // const result = this.searchPath;
-    // this.searchPath = [];
-    // return result;
