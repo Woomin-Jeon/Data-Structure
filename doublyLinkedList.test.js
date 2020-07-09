db6f48be4debcc282c41ffedfab48f397bdc483b
woomin <dal96k@gmail.com>
2020-07-09 13:56:17 +0900

    Update DoublyLinkedList class

--- doublyLinkedList.test.js
+++ doublyLinkedList.test.js
@@ -4,126 +4,169 @@ class Node {
     this.prev = null;
     this.next = null;
   }
+
+  setPrev(prev) {
+    this.prev = prev;
+  }
+
+  setNext(next) {
+    this.next = next;
+  }
+
+  getValue() {
+    return this.value;
+  }
 }
 
 class DoublyLinkedList {
   constructor() {
-    this.head = null;
-    this.tail = null;
+    this.head = null
+    this.tail = null
   }
 
-  isFirstNode(node) {
-    if (!this.head) {
-      this.head = node;
-    }    
+  append(node) {
     if (!this.tail) {
-      this.tail = node;
+      this.createHeadTail(node);
+      return;
     }
-  }
-
-  append(value) {
-    const currentNode = new Node(value);
-    this.isFirstNode(currentNode);
-    const previousNode = this.tail;
-    previousNode.next = currentNode;
-
-    currentNode.prev = previousNode;
     
-    this.tail = currentNode;
+    const pastTail = this.tail;
+    this.tail = node;
+    
+    pastTail.setNext(node);
+    node.setPrev(pastTail);
   }
 
-  prepend(value) {
-    const node = new Node(value);
-    this.isFirstNode(node);
+  prepend(node) {
+    if (!this.head) {
+      this.createHeadTail(node);
+      return;
+    }
+
+    const pastHead = this.head;
     this.head = node;
-  }
 
-  remove(value) {
-    const target = this.find(value);
-    target.prev.next = target.next;
-    target.next.prev = target.prev;
+    pastHead.setPrev(node);
+    node.setNext(pastHead);
   }
 
   isEmpty() {
-    return !this.tail;
+    return !this.head;
   }
 
-  find(value) {
+  has(value) {
     let currentNode = this.head;
-
-    while (currentNode) {
-      if (currentNode.value === value) {
+    while (currentNode) {      
+      if (currentNode.getValue() === value) {
         return currentNode;
       }
+
       currentNode = currentNode.next;
     }
 
     return null;
   }
 
+  size() {
+    let node = this.head;
+    let count = 0;
+
+    while(node) {
+      count += 1;
+      node = node.next;
+    }
+
+    return count;
+  }
+
+  remove(value) {
+    const node = this.has(value);
+    if (!node) {
+      return null;
+    }
+
+    const prevNode = node.prev;
+    const nextNode = node.next;
+
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
+  }
+
+  createHeadTail(node) {
+    this.head = node;
+    this.tail = node;
+  }
+
   getHead() {
-    return this.head.value;
+    return this.head;
   }
 
   getTail() {
-    return this.tail.value;
+    return this.tail;
   }
 }
 
 test('append', () => {
   const list = new DoublyLinkedList();
 
-  list.append(1);
-  expect(list.getTail()).toBe(1);
+  list.append(new Node(1));
+  expect(list.getTail().getValue()).toBe(1);
 
-  list.append(2);
-  expect(list.getTail()).toBe(2);
+  list.append(new Node(2));
+  expect(list.getTail().getValue()).toBe(2);
 });
 
 test('prepend', () => {
   const list = new DoublyLinkedList();
 
-  list.prepend(1);
-  expect(list.getHead()).toBe(1);
+  list.prepend(new Node(1));
+  expect(list.getHead().getValue()).toBe(1);
 
-  list.prepend(2);
-  expect(list.getHead()).toBe(2);
-
-  expect(list.getTail()).toBe(1);
+  list.prepend(new Node(2));
+  expect(list.getHead().getValue()).toBe(2);
 });
 
 test('isEmpty', () => {
   const list = new DoublyLinkedList();
 
-  expect(list.isEmpty()).toBeTruthy();
+  expect(list.isEmpty()).toBe(true);
+  
   list.append(1);
-  expect(list.isEmpty()).toBeFalsy();
+  expect(list.isEmpty()).toBe(false);
 });
 
-test('find', () => {
+test('has', () => {
   const list = new DoublyLinkedList();
 
-  list.append(2);
-  list.append(3);
+  list.append(new Node(2));
+  list.append(new Node(4));
 
-  expect(list.find(2)).toBeDefined();
-  expect(list.find(3)).toBeDefined();
+  expect(list.has(2)).toBeDefined();
+  expect(list.has(3)).toBeNull();
+  expect(list.has(4)).toBeDefined();
+});
 
-  let node = list.find(2);
-  expect(node.value).toBe(2);
-  node = list.find(3);
-  expect(node.value).toBe(3);
+test('size', () => {
+  const list = new DoublyLinkedList();
+
+  list.append(new Node(1));
+  list.append(new Node(2));
+  list.append(new Node(3));
+
+  expect(list.size()).toBe(3);
 });
 
 test('remove', () => {
   const list = new DoublyLinkedList();
 
-  list.append(1);
-  list.append(2);
-  list.append(3);
+  list.append(new Node(1));
+  list.append(new Node(2));
+  list.append(new Node(3));
 
   list.remove(2);
-  expect(list.find(2)).toBeNull();
+
+  expect(list.has(2)).toBeNull();
+  expect(list.size()).toBe(2);
 });
 
 module.exports = DoublyLinkedList;
